c4cec711901001e548685b0284c9139cb9e055ca
tejasvijb <jbtejasvi@gmail.com>
2024-07-12 05:00:56 +0530

    setup api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { Questions, Quizzes } from "./db";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/quizzes", (req: Request, res: Response) => {
+  res.send({ data: Quizzes })
+});
+
+app.get('/quizzes/:id', (req, res) => {
+    const quizId = req.params.id;
+    const quiz = Quizzes.find(q => q.id === quizId);
+  
+    if (quiz) {
+      res.json(quiz);
+    } else {
+      res.status(404).json({ message: 'Quiz not found' });
+    }
+});
+
+app.get('/quizzes/:id/questions', (req, res) => {
+    const quizId = req.params.id;
+    const quiz = Quizzes.find(q => q.id === quizId);
+
+    if (quiz) {
+      res.json(Questions);
+    } else {
+      res.status(404).json({ message: 'Quiz not found' });
+    }
 });
 
 app.listen(port, () => {
