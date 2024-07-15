d900c1af1c8532893001cdd7cf8f803f1ced8cd9
tejasvijb <jbtejasvi@gmail.com>
2024-07-15 23:55:42 +0530

    set up routers page

---
+++ routes/quizzes.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from "express";
+import { Questions, Quizzes } from "../src/db";
+
+const quizzesRouter = express.Router()
+
+
+
+// write api for "/" return a empty json
+quizzesRouter.get("/", (req: Request, res: Response) => {
+    res.send({ data: Quizzes })
+})
+
+quizzesRouter.get("/:id", (req: Request, res: Response) => {
+    const quizId = req.params.id;
+    const quiz = Quizzes.find(q => q.id === quizId);
+  
+    if (quiz) {
+      res.json(quiz);
+    } else {
+      res.status(404).json({ message: 'Quiz not found' });
+    }
+})
+
+quizzesRouter.get('/quizzes/:id/questions', (req, res) => {
+    const quizId = req.params.id;
+    const quiz = Quizzes.find(q => q.id === quizId);
+
+    if (quiz) {
+      res.json(Questions);
+    } else {
+      res.status(404).json({ message: 'Quiz not found' });
+    }
+});
+  
+
+export default quizzesRouter
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,14 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { Questions, Quizzes } from "./db";
+import quizzesRouter from "../routes/quizzes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/quizzes", (req: Request, res: Response) => {
-  res.send({ data: Quizzes })
-});
-
-app.get('/quizzes/:id', (req, res) => {
-    const quizId = req.params.id;
-    const quiz = Quizzes.find(q => q.id === quizId);
-  
-    if (quiz) {
-      res.json(quiz);
-    } else {
-      res.status(404).json({ message: 'Quiz not found' });
-    }
-});
-
-app.get('/quizzes/:id/questions', (req, res) => {
-    const quizId = req.params.id;
-    const quiz = Quizzes.find(q => q.id === quizId);
-
-    if (quiz) {
-      res.json(Questions);
-    } else {
-      res.status(404).json({ message: 'Quiz not found' });
-    }
-});
+app.use("/api/quizzes", quizzesRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
